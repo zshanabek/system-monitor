3660955ce8b2f6f73727fbdb4742b58d02137b29
Zhunissali <zshanabek@gmail.com>
2018-10-14 21:37:19 +0300

    [Add] - cat

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,76 @@
 #include "../includes/CPU.hpp"
 #include "../includes/Network.hpp"
 #include "../includes/Module.hpp"
+#include <ctime>
+
+unsigned int cat = 0;
+
+void drawcat()
+{
+	cat = (cat == 3) ? 0 : cat + 1; 
+	attron(COLOR_PAIR(1));
+	rectangle(32, 0, 40, 50);
+	attroff(COLOR_PAIR(1));
+	std::string array[50];
+
+	int i;
+	i = 0;
+
+	int a;
+	a = 33;
+	if (cat == 0)
+	{
+		array[0] = "  )\\._.,--....,'``.       ";
+		array[1] = " /,   _.. \\   _\\  (`._ ,.";
+		array[2] = "`._.-(,_..'--(,_..'`-.;.'  ";
+		array[3] = "                           ";
+
+		while(i < 4)
+		{
+			mvprintw(a + i, 2, array[i].c_str());
+			i++;
+		}
+	}
+	else if (cat == 1)
+	{
+		array[0] = "  |\\      _,,,---,,_      ";
+		array[1] = "  /,`.-'`'    -.  ;-;;,_   ";
+		array[2] = " |,4-  ) )-,_..;\\ (  `'-' ";
+		array[3] = "'---''(_/--'  `-'\\_)      ";
+		i = 0;
+		while(i < 4)
+		{
+			mvprintw(a + i, 2, array[i].c_str());
+			i++;
+		}
+	}
+	else if (cat == 2)
+	{
+		array[0] = "  |\\      _,,,,--,,_      ";
+		array[1] = "  /,`.-'`'    -,  ;-;,     ";
+		array[2] = " |,4-  ) ),,__ ) /;  ;;    ";
+		array[3] = "'---''(.'--'  (.'`.) `'    ";
+		i = 0;
+		while(i < 4)
+		{
+			mvprintw(a + i, 2, array[i].c_str());
+			i++;
+		}
+	}
+	else if (cat == 3)
+	{
+		array[0] = "  |\\      _,,,,--,,_      ";
+		array[1] = "  /,`.-'`'    -,  \\-;,    ";
+		array[2] = " |,4-  ) ),,__ ,\\ (  ;;   ";
+		array[3] = "'---''(.'--'  `-'`.)`'     ";
+		i = 0;
+		while(i < 4)
+		{
+			mvprintw(a + i, 2, array[i].c_str());
+			i++;
+		}
+	}
+}
 
 void terminal()
 {
@@ -27,19 +97,26 @@ void terminal()
 	m.pushback(&ram);
 	m.pushback(&cpu);
 	m.pushback(&net);
+	int frametime = 1000 / 200;
 
 	start_color();
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 	nodelay(stdscr, true);
 	while (!exit)
 	{
+		clock_t time = clock();
 		command = getch();
 		if (27 == command)
 			exit = true;
 		m.updateData();
 		m.showData();
+		drawcat();
 		refresh();
+		while (clock() < time + frametime)
+  		 ;
+
 	}
+	
 	endwin();
 }
 
